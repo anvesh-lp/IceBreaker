12c02fa876b07a041c135a4e93dd4d9d27970f6d
anvesh-lp <knanvesh.01@gmail.com>
2022-09-22 21:10:48 -0400

    Adding binding between view and controller

--- app/src/main/java/com/example/icebreaker/MainActivity.kt
+++ app/src/main/java/com/example/icebreaker/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.icebreaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputBinding
+import com.example.icebreaker.databinding.ActivityMainBinding
+
+private const val TAG = "Ice breaker android fall 2022"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.randombutton.setOnClickListener{
+            Log.d(TAG,"Random Button pressed")
+        }
+        binding.submitbutton.setOnClickListener  {
+            Log.d(TAG,"Question Button pressed")
+        }
+
+
     }
 }
